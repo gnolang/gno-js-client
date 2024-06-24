67d3b648c12ce60dcf5cb1b6866043c07335b762
jinoosss <jwchoi@onbloc.xyz>
2024-06-24 12:22:16 +0900

    feat: add the changed syntax of the ABCI query

--- src/provider/utility/provider.utility.ts
+++ src/provider/utility/provider.utility.ts
@@ -2,15 +2,41 @@ import { stringToBase64 } from '@gnolang/tm2-js-client';
 
 /**
  * Prepares the VM ABCI query params by concatenating them
- * with a newline separation and encoding them to base64
+ * with new line or "." or ":" characters separation and encoding them to base64
+ * `evaluateExpression` uses the "." character to separate parameters.
+ * `getRenderOutput` uses the ":" character to separate parameters.
  * @param {string[]} params the params for the ABCI call
+ * @param {string} separator the separator for ABCI call parameters (default: "\n")
  */
-export const prepareVMABCIQuery = (params: string[]): string => {
+export const prepareVMABCIQuery = (
+  params: string[],
+  separator = '\n'
+): string => {
   if (params.length == 1) {
     return stringToBase64(params[0]);
   }
 
-  return stringToBase64(params.join('\n'));
+  return stringToBase64(params.join(separator));
+};
+
+/**
+ * Prepare the VM ABCI `evaluateExpression` query parameters by concatenating them
+ * with the "." character delimiter and encoding them with base64.
+ * @param {string[]} params the params for the ABCI call
+ */
+export const prepareVMABCIEvaluateExpressionQuery = (
+  params: string[]
+): string => {
+  return prepareVMABCIQuery(params, '.');
+};
+
+/**
+ * Prepare the VM ABCI `render` query parameters by concatenating them
+ * with the ":" character delimiter and encoding them with base64.
+ * @param {string[]} params the params for the ABCI call
+ */
+export const prepareVMABCIRenderQuery = (params: string[]): string => {
+  return prepareVMABCIQuery(params, ':');
 };
 
 export const extractStringFromResponse = (abciData: string | null): string => {
