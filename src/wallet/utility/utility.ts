b1a85b373f978e820d32e9efce1225c05b184795
Alan R. Soares <alanrsoars@gmail.com>
2025-08-20 10:45:59 +1200

    feat: enhance transaction message decoding with typed output

--- src/wallet/utility/utility.ts
+++ src/wallet/utility/utility.ts
@@ -32,11 +32,17 @@ export const fundsToCoins = (funds?: Map<string, number>): string => {
  */
 export const defaultTxFee = '1000000ugnot'; // 1 GNOT
 
+export type DecodedMessage<
+  T extends Record<string, unknown> = Record<string, unknown>,
+> = T & {
+  '@type': MsgEndpoint;
+};
+
 /**
  * Decodes (and unrolls) Transaction messages into full objects
  * @param {Any[]} messages the encoded transaction messages
  */
-export const decodeTxMessages = (messages: Any[]): any[] => {
+export const decodeTxMessages = (messages: Any[]): DecodedMessage[] => {
   return messages.map((m: Any) => {
     switch (m.type_url) {
       case MsgEndpoint.MSG_CALL: {
