a5d2fc50d0e1c564b8e5e1f00c8ddeaad6a56ea6
jeronimoalbi <894299+jeronimoalbi@users.noreply.github.com>
2025-08-19 15:45:15 +0200

    chore: change proto generate script to apply compatiblity patch

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -15,3 +15,17 @@ for x in ${FILES}; do
     --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=messages,useDate=false,snakeToCamel=false,emitDefaultValues=json-methods" \
     ${x}
 done
+
+# Apply amino JSON compatibility changes to patch proto generated files.
+# Patch fixes two issues:
+# - Empty `max_deposit` is represented in JSON as an empty string in proto while omitted completely in Amino JSON
+# - Empty `google.protobuf.Any` fields are omitted in proto JSON while explicitly set to null in Amino JSON
+#
+# See: https://github.com/gnolang/gno-js-client/pull/184
+#
+# This is a short term solution that potentially requires maintaining the patch updated.
+if [ $? -eq 0 ]; then
+  # First prettify generated code to allow the patch to be applied
+  ./node_modules/.bin/prettier --write ${OUT_DIR}/gno/vm.ts && \
+  git apply ./scripts/amino-json-compatibility.patch
+fi
